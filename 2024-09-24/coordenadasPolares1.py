30b1c7e81688cb2984a7cc6e9355f543fd4a40a4
TomAintAround <tcorreia2005@proton.me>
2024-09-26 11:00:50 +0100

    fix(2024-09-24): add workarounds for when x <= 0

--- 2024-09-24/coordenadasPolares1.py
+++ 2024-09-24/coordenadasPolares1.py
@@ -1,11 +1,22 @@
 # Exercício 1.17
 
-from math import atan, sqrt, pow
+from math import atan, sqrt, pow, pi
 
 x = eval(input("Qual é a abcissa x? "))
 y = eval(input("Qual é a ordenada y? "))
 
-angulo = atan(y / x)
+if x == 0:
+    if y > 0:
+        angulo = pi / 2
+    elif y < 0:
+        angulo = 3 * pi / 2
+    else:
+        angulo = 0
+elif x < 0:
+    angulo = atan(y / x) + pi
+else:
+    angulo = atan(y / x)
+
 distancia = sqrt(pow(x, 2) + pow(y, 2))
 
 print("Essas coordenadas convertidas para polar são ({}, {}).".format(distancia, angulo))
--- 2024-09-24/coordenadasPolares2.py
+++ 2024-09-24/coordenadasPolares2.py
@@ -1,10 +1,22 @@
 # Exercício 1.17
 
-from math import atan, sqrt, pow
+from math import atan, sqrt, pow, pi
 
 def polar(x, y):
     distancia = sqrt(pow(x, 2) + pow(y, 2))
-    angulo = atan(y / x)
+
+    if x == 0:
+        if y > 0:
+            angulo = pi / 2
+        elif y < 0:
+            angulo = 3 * pi / 2
+        else:
+            angulo = 0
+    elif x < 0:
+        angulo = atan(y / x) + pi
+    else:
+        angulo = atan(y / x)
+
     return (distancia, angulo)
 
 x = eval(input("Qual é a abcissa x? "))
