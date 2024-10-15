1f8454ecbb2413c6200e31607b40674c8aa9b869
TomAintAround <tcorreia2005@proton.me>
2024-10-15 19:11:41 +0100

    fix(2024-10-11/euler.py): make it accept the number 0 as input

--- 2024-10-11/euler.py
+++ 2024-10-11/euler.py
@@ -2,12 +2,12 @@
 
 from math import factorial
 
-precisao = 0
-while precisao <= 0:
-    precisao = int(input('Escola um número natural. Quanto maior for, maior será a precisão: ').strip())
+precisao = -1
+while precisao < 0:
+    precisao = int(input('Escola um número natural ou 0. Quanto maior for, maior será a precisão: ').strip())
 
 e = 0
-for i in range(precisao):
+for i in range(precisao + 1):
     e += 1 / factorial(i)
 
 print('O número aproximado de e é:\n{}'.format(e))
