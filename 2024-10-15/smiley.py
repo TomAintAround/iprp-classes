8bdd6a598734e3cbd62afbecbd4475949e1382f6
TomAintAround <tcorreia2005@proton.me>
2024-10-18 20:30:15 +0100

    refractor(2024-10-15/smiley.py): use appropriate turtle functions

--- 2024-10-15/smiley.py
+++ 2024-10-15/smiley.py
@@ -5,21 +5,16 @@ import turtle
 def smiley(raio):
     def olhos(raioCirculo, multiplicador):
         if multiplicador <= 1:
-            turtle.penup()
-            turtle.goto(multiplicador * 50, raioCirculo)
-            turtle.begin_fill()
-            turtle.circle(raioCirculo / 10)
-            turtle.end_fill()
+            turtle.teleport(multiplicador * 50, raioCirculo)
+            turtle.dot(raioCirculo / 10 * 2)
             return olhos(raioCirculo, multiplicador + 2)
         else:
             turtle.pendown()
             return None
     
     def boca(raioCirculo):
-        turtle.penup()
         turtle.right(15)
-        turtle.goto(-1 * raioCirculo + raioCirculo / 2, raioCirculo / 2)
-        turtle.pendown()
+        turtle.teleport(-1 * raioCirculo + raioCirculo / 2, raioCirculo / 2)
         turtle.circle(raioCirculo * 2, 30)
 
     turtle.hideturtle()
